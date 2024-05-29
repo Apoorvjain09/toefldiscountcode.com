279bff79965ec005a3877db65b341db03359eeae
Apoorv <113705783+Apoorvjain09@users.noreply.github.com>
2024-05-29 16:36:07 +0530

    add profile

---
+++ app/profile/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+import React from 'react';
+import { UserProfile, useAuth, SignIn } from '@clerk/nextjs';
+
+function Page() {
+  const { isSignedIn } = useAuth();
+  return (
+    <div className='xl:mt-10 xl:mb-10 flex gap-7 flex-wrap items-center justify-center'>
+      {isSignedIn ? <div><UserProfile /></div> : <div><SignIn /></div>}
+    </div>
+  );
+}
+
+export default Page;

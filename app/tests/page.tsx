cd5f4f211ab0308154fc097834f8c22ca9bfd771
Apoorv <113705783+Apoorvjain09@users.noreply.github.com>
2024-05-30 14:26:21 +0530

    addded globe

--- app/tests/page.tsx
+++ app/tests/page.tsx
@@ -5,42 +5,401 @@ import { Boxes } from "@/components/ui/backgroundBoxes";
 import { cn } from "@/lib/utils";
 import { motion } from "framer-motion";
 import { LampContainer } from "@/components/ui/lamp";
+import dynamic from "next/dynamic";
 
-const testimonials = [
-    {
-        quote:
-            "It was the best of times, it was the worst of times, it was the age of wisdom, it was the age of foolishness, it was the epoch of belief, it was the epoch of incredulity, it was the season of Light, it was the season of Darkness, it was the spring of hope, it was the winter of despair.",
-        name: "Charles Dickens",
-        title: "A Tale of Two Cities",
-    },
-    {
-        quote:
-            "To be, or not to be, that is the question: Whether 'tis nobler in the mind to suffer The slings and arrows of outrageous fortune, Or to take Arms against a Sea of troubles, And by opposing end them: to die, to sleep.",
-        name: "William Shakespeare",
-        title: "Hamlet",
-    },
-    {
-        quote: "All that we see or seem is but a dream within a dream.",
-        name: "Edgar Allan Poe",
-        title: "A Dream Within a Dream",
-    },
-    {
-        quote:
-            "It is a truth universally acknowledged, that a single man in possession of a good fortune, must be in want of a wife.",
-        name: "Jane Austen",
-        title: "Pride and Prejudice",
-    },
-    {
-        quote:
-            "Call me Ishmael. Some years ago—never mind how long precisely—having little or no money in my purse, and nothing particular to interest me on shore, I thought I would sail about a little and see the watery part of the world.",
-        name: "Herman Melville",
-        title: "Moby-Dick",
-    },
-];
+const World = dynamic(() => import("@/components/ui/globe").then((m) => m.World), {
+    ssr: false,
+});
 
 export default function Page() {
-    const [showTests, setShowTests] = useState(false);
 
+    const globeConfig = {
+        pointSize: 4,
+        globeColor: "#062056",
+        showAtmosphere: true,
+        atmosphereColor: "#FFFFFF",
+        atmosphereAltitude: 0.1,
+        emissive: "#062056",
+        emissiveIntensity: 0.1,
+        shininess: 0.9,
+        polygonColor: "rgba(255,255,255,0.7)",
+        ambientLight: "#38bdf8",
+        directionalLeftLight: "#ffffff",
+        directionalTopLight: "#ffffff",
+        pointLight: "#ffffff",
+        arcTime: 1000,
+        arcLength: 0.9,
+        rings: 1,
+        maxRings: 3,
+        initialPosition: { lat: 22.3193, lng: 114.1694 },
+        autoRotate: true,
+        autoRotateSpeed: 0.5,
+    };
+    const colors = ["#06b6d4", "#3b82f6", "#6366f1"];
+    const sampleArcs = [
+        {
+            order: 1,
+            startLat: -19.885592,
+            startLng: -43.951191,
+            endLat: -22.9068,
+            endLng: -43.1729,
+            arcAlt: 0.1,
+            color: colors[Math.floor(Math.random() * (colors.length - 1))],
+        },
+        {
+            order: 1,
+            startLat: 28.6139,
+            startLng: 77.209,
+            endLat: 3.139,
+            endLng: 101.6869,
+            arcAlt: 0.2,
+            color: colors[Math.floor(Math.random() * (colors.length - 1))],
+        },
+        {
+            order: 1,
+            startLat: -19.885592,
+            startLng: -43.951191,
+            endLat: -1.303396,
+            endLng: 36.852443,
+            arcAlt: 0.5,
+            color: colors[Math.floor(Math.random() * (colors.length - 1))],
+        },
+        {
+            order: 2,
+            startLat: 1.3521,
+            startLng: 103.8198,
+            endLat: 35.6762,
+            endLng: 139.6503,
+            arcAlt: 0.2,
+            color: colors[Math.floor(Math.random() * (colors.length - 1))],
+        },
+        {
+            order: 2,
+            startLat: 51.5072,
+            startLng: -0.1276,
+            endLat: 3.139,
+            endLng: 101.6869,
+            arcAlt: 0.3,
+            color: colors[Math.floor(Math.random() * (colors.length - 1))],
+        },
+        {
+            order: 2,
+            startLat: -15.785493,
+            startLng: -47.909029,
+            endLat: 36.162809,
+            endLng: -115.119411,
+            arcAlt: 0.3,
+            color: colors[Math.floor(Math.random() * (colors.length - 1))],
+        },
+        {
+            order: 3,
+            startLat: -33.8688,
+            startLng: 151.2093,
+            endLat: 22.3193,
+            endLng: 114.1694,
+            arcAlt: 0.3,
+            color: colors[Math.floor(Math.random() * (colors.length - 1))],
+        },
+        {
+            order: 3,
+            startLat: 21.3099,
+            startLng: -157.8581,
+            endLat: 40.7128,
+            endLng: -74.006,
+            arcAlt: 0.3,
+            color: colors[Math.floor(Math.random() * (colors.length - 1))],
+        },
+        {
+            order: 3,
+            startLat: -6.2088,
+            startLng: 106.8456,
+            endLat: 51.5072,
+            endLng: -0.1276,
+            arcAlt: 0.3,
+            color: colors[Math.floor(Math.random() * (colors.length - 1))],
+        },
+        {
+            order: 4,
+            startLat: 11.986597,
+            startLng: 8.571831,
+            endLat: -15.595412,
+            endLng: -56.05918,
+            arcAlt: 0.5,
+            color: colors[Math.floor(Math.random() * (colors.length - 1))],
+        },
+        {
+            order: 4,
+            startLat: -34.6037,
+            startLng: -58.3816,
+            endLat: 22.3193,
+            endLng: 114.1694,
+            arcAlt: 0.7,
+            color: colors[Math.floor(Math.random() * (colors.length - 1))],
+        },
+        {
+            order: 4,
+            startLat: 51.5072,
+            startLng: -0.1276,
+            endLat: 48.8566,
+            endLng: -2.3522,
+            arcAlt: 0.1,
+            color: colors[Math.floor(Math.random() * (colors.length - 1))],
+        },
+        {
+            order: 5,
+            startLat: 14.5995,
+            startLng: 120.9842,
+            endLat: 51.5072,
+            endLng: -0.1276,
+            arcAlt: 0.3,
+            color: colors[Math.floor(Math.random() * (colors.length - 1))],
+        },
+        {
+            order: 5,
+            startLat: 1.3521,
+            startLng: 103.8198,
+            endLat: -33.8688,
+            endLng: 151.2093,
+            arcAlt: 0.2,
+            color: colors[Math.floor(Math.random() * (colors.length - 1))],
+        },
+        {
+            order: 5,
+            startLat: 34.0522,
+            startLng: -118.2437,
+            endLat: 48.8566,
+            endLng: -2.3522,
+            arcAlt: 0.2,
+            color: colors[Math.floor(Math.random() * (colors.length - 1))],
+        },
+        {
+            order: 6,
+            startLat: -15.432563,
+            startLng: 28.315853,
+            endLat: 1.094136,
+            endLng: -63.34546,
+            arcAlt: 0.7,
+            color: colors[Math.floor(Math.random() * (colors.length - 1))],
+        },
+        {
+            order: 6,
+            startLat: 37.5665,
+            startLng: 126.978,
+            endLat: 35.6762,
+            endLng: 139.6503,
+            arcAlt: 0.1,
+            color: colors[Math.floor(Math.random() * (colors.length - 1))],
+        },
+        {
+            order: 6,
+            startLat: 22.3193,
+            startLng: 114.1694,
+            endLat: 51.5072,
+            endLng: -0.1276,
+            arcAlt: 0.3,
+            color: colors[Math.floor(Math.random() * (colors.length - 1))],
+        },
+        {
+            order: 7,
+            startLat: -19.885592,
+            startLng: -43.951191,
+            endLat: -15.595412,
+            endLng: -56.05918,
+            arcAlt: 0.1,
+            color: colors[Math.floor(Math.random() * (colors.length - 1))],
+        },
+        {
+            order: 7,
+            startLat: 48.8566,
+            startLng: -2.3522,
+            endLat: 52.52,
+            endLng: 13.405,
+            arcAlt: 0.1,
+            color: colors[Math.floor(Math.random() * (colors.length - 1))],
+        },
+        {
+            order: 7,
+            startLat: 52.52,
+            startLng: 13.405,
+            endLat: 34.0522,
+            endLng: -118.2437,
+            arcAlt: 0.2,
+            color: colors[Math.floor(Math.random() * (colors.length - 1))],
+        },
+        {
+            order: 8,
+            startLat: -8.833221,
+            startLng: 13.264837,
+            endLat: -33.936138,
+            endLng: 18.436529,
+            arcAlt: 0.2,
+            color: colors[Math.floor(Math.random() * (colors.length - 1))],
+        },
+        {
+            order: 8,
+            startLat: 49.2827,
+            startLng: -123.1207,
+            endLat: 52.3676,
+            endLng: 4.9041,
+            arcAlt: 0.2,
+            color: colors[Math.floor(Math.random() * (colors.length - 1))],
+        },
+        {
+            order: 8,
+            startLat: 1.3521,
+            startLng: 103.8198,
+            endLat: 40.7128,
+            endLng: -74.006,
+            arcAlt: 0.5,
+            color: colors[Math.floor(Math.random() * (colors.length - 1))],
+        },
+        {
+            order: 9,
+            startLat: 51.5072,
+            startLng: -0.1276,
+            endLat: 34.0522,
+            endLng: -118.2437,
+            arcAlt: 0.2,
+            color: colors[Math.floor(Math.random() * (colors.length - 1))],
+        },
+        {
+            order: 9,
+            startLat: 22.3193,
+            startLng: 114.1694,
+            endLat: -22.9068,
+            endLng: -43.1729,
+            arcAlt: 0.7,
+            color: colors[Math.floor(Math.random() * (colors.length - 1))],
+        },
+        {
+            order: 9,
+            startLat: 1.3521,
+            startLng: 103.8198,
+            endLat: -34.6037,
+            endLng: -58.3816,
+            arcAlt: 0.5,
+            color: colors[Math.floor(Math.random() * (colors.length - 1))],
+        },
+        {
+            order: 10,
+            startLat: -22.9068,
+            startLng: -43.1729,
+            endLat: 28.6139,
+            endLng: 77.209,
+            arcAlt: 0.7,
+            color: colors[Math.floor(Math.random() * (colors.length - 1))],
+        },
+        {
+            order: 10,
+            startLat: 34.0522,
+            startLng: -118.2437,
+            endLat: 31.2304,
+            endLng: 121.4737,
+            arcAlt: 0.3,
+            color: colors[Math.floor(Math.random() * (colors.length - 1))],
+        },
+        {
+            order: 10,
+            startLat: -6.2088,
+            startLng: 106.8456,
+            endLat: 52.3676,
+            endLng: 4.9041,
+            arcAlt: 0.3,
+            color: colors[Math.floor(Math.random() * (colors.length - 1))],
+        },
+        {
+            order: 11,
+            startLat: 41.9028,
+            startLng: 12.4964,
+            endLat: 34.0522,
+            endLng: -118.2437,
+            arcAlt: 0.2,
+            color: colors[Math.floor(Math.random() * (colors.length - 1))],
+        },
+        {
+            order: 11,
+            startLat: -6.2088,
+            startLng: 106.8456,
+            endLat: 31.2304,
+            endLng: 121.4737,
+            arcAlt: 0.2,
+            color: colors[Math.floor(Math.random() * (colors.length - 1))],
+        },
+        {
+            order: 11,
+            startLat: 22.3193,
+            startLng: 114.1694,
+            endLat: 1.3521,
+            endLng: 103.8198,
+            arcAlt: 0.2,
+            color: colors[Math.floor(Math.random() * (colors.length - 1))],
+        },
+        {
+            order: 12,
+            startLat: 34.0522,
+            startLng: -118.2437,
+            endLat: 37.7749,
+            endLng: -122.4194,
+            arcAlt: 0.1,
+            color: colors[Math.floor(Math.random() * (colors.length - 1))],
+        },
+        {
+            order: 12,
+            startLat: 35.6762,
+            startLng: 139.6503,
+            endLat: 22.3193,
+            endLng: 114.1694,
+            arcAlt: 0.2,
+            color: colors[Math.floor(Math.random() * (colors.length - 1))],
+        },
+        {
+            order: 12,
+            startLat: 22.3193,
+            startLng: 114.1694,
+            endLat: 34.0522,
+            endLng: -118.2437,
+            arcAlt: 0.3,
+            color: colors[Math.floor(Math.random() * (colors.length - 1))],
+        },
+        {
+            order: 13,
+            startLat: 52.52,
+            startLng: 13.405,
+            endLat: 22.3193,
+            endLng: 114.1694,
+            arcAlt: 0.3,
+            color: colors[Math.floor(Math.random() * (colors.length - 1))],
+        },
+        {
+            order: 13,
+            startLat: 11.986597,
+            startLng: 8.571831,
+            endLat: 35.6762,
+            endLng: 139.6503,
+            arcAlt: 0.3,
+            color: colors[Math.floor(Math.random() * (colors.length - 1))],
+        },
+        {
+            order: 13,
+            startLat: -22.9068,
+            startLng: -43.1729,
+            endLat: -34.6037,
+            endLng: -58.3816,
+            arcAlt: 0.1,
+            color: colors[Math.floor(Math.random() * (colors.length - 1))],
+        },
+        {
+            order: 14,
+            startLat: -33.936138,
+            startLng: 18.436529,
+            endLat: 21.395643,
+            endLng: 39.883798,
+            arcAlt: 0.3,
+            color: colors[Math.floor(Math.random() * (colors.length - 1))],
+        },
+    ];
+
+    const [showTests, setShowTests] = useState(false);
     const handleGetStartedClick = () => {
         setShowTests(true);
     };
@@ -49,14 +408,6 @@ export default function Page() {
         <>
             {!showTests ? (
                 <div>
-                    {/* <section id="home" className="relative py-20 bg-gradient-to-r from-blue-500 to-green-500 overflow-hidden">
-                        <div className="absolute inset-0 bg-slate-400 opacity-70"></div>
-                        <div className="container mx-auto text-center relative z-10">
-                            <h1 className="text-4xl font-bold mb-4 text-white">Ace Your TOEFL Test with Our AI-Powered Mock Tests</h1>
-                            <p className="text-xl mb-8 text-white">Practice with realistic tests and get detailed feedback to improve your scores.</p>
-                            <button onClick={handleGetStartedClick} className="bg-blue-600 text-white py-2 px-4 rounded">Get Started</button>
-                        </div>
-                    </section> */}
                     <div className="h-96 relative w-full overflow-hidden bg-slate-900 flex flex-col items-center justify-center rounded-lg">
                         <div className="absolute inset-0 w-full h-full bg-slate-900 z-20 [mask-image:radial-gradient(transparent,white)] pointer-events-none" />
                         <Boxes />
@@ -102,26 +453,43 @@ export default function Page() {
                                 duration: 0.8,
                                 ease: "easeInOut",
                             }}
-                            className="mt-8 bg-gradient-to-br from-slate-300 to-slate-500 py-4 bg-clip-text text-center text-4xl font-medium tracking-tight text-transparent md:text-7xl"
+                            className="mt-8 bg-gradient-to-br from-slate-300 to-slate-500 py-4 bg-clip-text text-center text-5xl font-medium tracking-tight text-transparent md:text-7xl"
                         >
-                            Build lamps <br /> the right way
+                            Increase your <br /> Toefl Score <br /> the right way
                         </motion.h1>
                     </LampContainer>
-                    <section id="testimonials" className="bg-gray-100 py-20">
-                        <div className="container mx-auto text-center">
-                            <h2 className="text-3xl font-bold mb-10">What Our Users Say</h2>
-                            <div className="flex flex-wrap justify-around">
-                                <div className="w-full md:w-1/3 p-4">
-                                    <div className="bg-white shadow p-6 rounded">
-                                        <p className="italic">`&quot;This mock test platform is amazing. The AI feedback helped me improve my scores significantly!`&quot;</p>
-                                        <p className="mt-4 font-bold">- User Name</p>
-                                    </div>
-                                </div>
+                    <div className="flex flex-row py-20 h-[70vh] sm:h-full bg-black relative w-full">
+                        <div className="max-w-7xl mx-auto w-full relative overflow-hidden h-full md:h-[100vh] px-4">
+                            <motion.div
+                                initial={{
+                                    opacity: 0,
+                                    y: 20,
+                                }}
+                                animate={{
+                                    opacity: 1,
+                                    y: 0,
+                                }}
+                                transition={{
+                                    duration: 1,
+                                }}
+                                className="div"
+                            >
+                                <h2 className="text-center text-xl md:text-4xl font-bold text-white">
+                                    We conduct toefl test worldwide
+                                </h2>
+                                <p className="text-center text-base md:text-lg font-normal text-neutral-200 max-w-md mt-2 mx-auto">
+                                    These tests are interactive and customizable. Have fun with it, and
+                                    don&apos;t forget to share it.
+                                </p>
+                            </motion.div>
+                            <div className="absolute w-full inset-x-0 h-40 pointer-events-none select-none z-40" />
+                            <div className="absolute w-full h-72 md:h-full z-10">
+                                <World data={sampleArcs} globeConfig={globeConfig} />;
                             </div>
                         </div>
-                    </section>
-                    <section id="cta" className="py-20 bg-blue-600 text-white text-center">
-                        <div className="container mx-auto">
+                    </div>
+                    <section id="cta" className="py-20 bg-black text-white text-center">
+                        <div className="container mx-auto gap-10">
                             <h2 className="text-3xl font-bold mb-4">Ready to Ace Your TOEFL Test?</h2>
                             <a href="#signup" className="bg-white text-blue-600 py-2 px-4 rounded">Sign Up Now</a>
                         </div>

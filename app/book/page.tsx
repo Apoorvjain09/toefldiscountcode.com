632187cb11f7844af9122b76442a9fdf0daa1411
apoorv <apoorvarnav1@gmail.com>
2024-07-25 15:29:16 +0530

    try seo methord 2

--- app/book/page.tsx
+++ app/book/page.tsx
@@ -1,19 +1,12 @@
 // app/book/page.tsx
 import React, { Suspense } from 'react';
 import LoadingSpinner from '@/components/ui/LoadingSpinner';
-import { Seo } from '@/components/Head/Seo';
 import Section from './Section';
 import { sections } from './sectionsData';
 
 const Page: React.FC = () => {
   return (
     <>
-      <Seo
-        title='TOEFL Books - Recommended Study Materials'
-        description='Find the best TOEFL books and study materials to prepare for your exam. Check our recommendations now!'
-        url='https://toeflgoglobal.com/book'
-        image="https://www.dropbox.com/scl/fi/efgh6d39t1z69ulz03dl3/GoGlobalSocialShare.jpg?rlkey=o8vttiq065fkpsemyzo04fcj5&raw=1"
-      />
       <Suspense fallback={<div><LoadingSpinner /></div>}>
         <div className='rounded-lg bg-gradient-to-r from-orange-500 to-pink-600'>
           <div className="text-center flex flex-col p-7">

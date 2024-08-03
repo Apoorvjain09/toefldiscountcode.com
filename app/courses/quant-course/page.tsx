43ecd47a813eb43100b8f3d5798a0018d53b7f28
Apoorv <113705783+Apoorvjain09@users.noreply.github.com>
2024-08-03 10:47:39 +0530

    finalize courses

--- app/courses/quant-course/page.tsx
+++ app/courses/quant-course/page.tsx
@@ -6,50 +6,60 @@ import { courseData, Lecture } from './courseData';
 import Sidebar from '@/app/vocabulary-course/sidebar';
 
 import VideoPlayer from './Videoplayer';
+import { useUser } from '@clerk/nextjs';
 
 const initialLecture = courseData.sections[0].chapters[0].lectures[0];
 
 const CoursePage: React.FC = () => {
   const [currentLecture, setCurrentLecture] = useState<Lecture>(initialLecture);
-  const thumbnailUrl = "https://www.dropbox.com/scl/fi/bnv3tio9lv26hfl6uxfa1/Quant_banner.png?rlkey=157scoylsfn6ocbks61vazrje&st=zby55ofw&raw=1"
+  const thumbnailUrl = "https://www.dropbox.com/scl/fi/rt471v44clzl8om5z2tcp/quant_banner_with_play.png?rlkey=7er8ivf0j0env6c6uhko4vxf9&st=h1zl20qg&raw=1"
+  const { user } = useUser();
+
   useEffect(() => {
-    // Disable right-click
-    const handleContextMenu = (event: MouseEvent) => {
+    //check foe metatada
+    if (user?.publicMetadata?.["Quant_course"] === "true") {
+      return
+    }else{
+      window.location.href = "/courses"
+    }
+
+  // Disable right-click
+  const handleContextMenu = (event: MouseEvent) => {
+    event.preventDefault();
+  };
+  document.addEventListener('contextmenu', handleContextMenu);
+
+  // Prevent opening DevTools using F12, Ctrl+Shift+I, etc.
+  const handleKeyDown = (event: KeyboardEvent) => {
+    if (
+      (event.ctrlKey && event.shiftKey && event.key === 'I') || // Ctrl+Shift+I
+      (event.ctrlKey && event.shiftKey && event.key === 'J') || // Ctrl+Shift+J
+      (event.ctrlKey && event.key === 'U') || // Ctrl+U
+      event.key === 'F12' // F12
+    ) {
       event.preventDefault();
-    };
-    document.addEventListener('contextmenu', handleContextMenu);
+    }
+  };
+  document.addEventListener('keydown', handleKeyDown);
 
-    // Prevent opening DevTools using F12, Ctrl+Shift+I, etc.
-    const handleKeyDown = (event: KeyboardEvent) => {
-      if (
-        (event.ctrlKey && event.shiftKey && event.key === 'I') || // Ctrl+Shift+I
-        (event.ctrlKey && event.shiftKey && event.key === 'J') || // Ctrl+Shift+J
-        (event.ctrlKey && event.key === 'U') || // Ctrl+U
-        event.key === 'F12' // F12
-      ) {
-        event.preventDefault();
-      }
-    };
-    document.addEventListener('keydown', handleKeyDown);
+  return () => {
+    document.removeEventListener('contextmenu', handleContextMenu);
+    document.removeEventListener('keydown', handleKeyDown);
+  };
+}, []);
 
-    return () => {
-      document.removeEventListener('contextmenu', handleContextMenu);
-      document.removeEventListener('keydown', handleKeyDown);
-    };
-  }, []);
-
-  return (
-    <div className="flex">
-      <Suspense fallback={<div>Loading Sidebar...</div>}>
-        <Sidebar sections={courseData.sections} setCurrentLecture={setCurrentLecture} />
+return (
+  <div className="flex">
+    <Suspense fallback={<div>Loading Sidebar...</div>}>
+      <Sidebar sections={courseData.sections} setCurrentLecture={setCurrentLecture} />
+    </Suspense>
+    <div className="w-full p-4">
+      <Suspense fallback={<div>Loading Video Player...</div>}>
+        <VideoPlayer videoUrl={currentLecture.videoUrl} thumbnailUrl={thumbnailUrl} />
       </Suspense>
-      <div className="w-full p-4">
-        <Suspense fallback={<div>Loading Video Player...</div>}>
-          <VideoPlayer videoUrl={currentLecture.videoUrl} thumbnailUrl={thumbnailUrl} />
-        </Suspense>
-      </div>
     </div>
-  );
+  </div>
+);
 };
 
 export default CoursePage;
--- app/courses/toefl-course/page.tsx
+++ app/courses/toefl-course/page.tsx
@@ -6,13 +6,22 @@ import { courseData, Lecture } from './courseData';
 import Sidebar from '@/app/vocabulary-course/sidebar';
 
 import VideoPlayer from './Videoplayer';
+import { useUser } from '@clerk/nextjs';
 
 const initialLecture = courseData.sections[0].chapters[0].lectures[0];
 
 const CoursePage: React.FC = () => {
   const [currentLecture, setCurrentLecture] = useState<Lecture>(initialLecture);
   const thumbnailUrl = "https://www.dropbox.com/scl/fi/myaawq2x8hl49t4vfvcez/toefl_banner_with_play.png?rlkey=rubea2fp2k881s50ob9t9mnwz&st=xta4nv44&raw=1"
+  const {user} = useUser()
+  
   useEffect(() => {
+    if (user?.publicMetadata?.["Quant_course"] === "true") {
+      return
+    }else{
+      window.location.href = "/courses"
+    }
+
     // Disable right-click
     const handleContextMenu = (event: MouseEvent) => {
       event.preventDefault();

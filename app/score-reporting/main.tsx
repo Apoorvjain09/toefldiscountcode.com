bb7be8c2658dd4f10af4523a65ce2798a6264fdd
apoorv <apoorv@mja.in>
2024-09-07 22:42:47 +0530

    voucher score reporting

--- app/score-reporting/main.tsx
+++ app/score-reporting/main.tsx
@@ -12,17 +12,17 @@ export default function Main() {
             <section className="bg-white dark:bg-gray-900 rounded-t-lg">
                 <div className="grid max-w-screen-xl px-4 pt-20 pb-8 mx-auto lg:gap-8 xl:gap-10 lg:py-16 lg:grid-cols-12 lg:pt-28">
                     <div className="mr-auto place-self-center lg:col-span-7">
-                        <h1 className="max-w-2xl mb-4 text-4xl font-extrabold leading-none tracking-tight md:text-4xl xl:text-5xl dark:text-white">Get Discount on TOEFL Score Reporting</h1>
-                        <h2 className="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">Get free Toefl Score reporting vouchers available for 2024. Send your TOEFL scores seamlessly to educational institutions. Additionaly <a href="https://mja.in" className="hover:underline">MJA</a> will help you with FREE application <a href="https://mja.in" className="hover:underline">Work</a>.</h2>
+                        <h1 className="max-w-2xl mb-4 text-4xl font-extrabold leading-none tracking-tight md:text-4xl xl:text-5xl dark:text-white">Get @50% Discount on TOEFL Score Reporting</h1>
+                        <h2 className="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">Get Toefl Score reporting vouchers available for 2024. Send your TOEFL scores seamlessly to educational institutions. Additionaly <a href="https://mja.in" className="hover:underline">MJA</a> will help you with FREE application <a href="https://mja.in" className="hover:underline">Work</a>.</h2>
                         <div className="space-y-4 sm:flex sm:space-y-0 sm:space-x-4">
-                            <a target="_blank" href="https://chat.whatsapp.com/CHwPiz6xEpHC0WSivb2UN7" className="inline-flex items-center justify-center w-full px-5 py-3 text-sm font-medium text-center text-gray-900 border border-gray-200 rounded-lg sm:w-auto hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800 gap-1">
+                            {/* <a target="_blank" href="https://chat.whatsapp.com/CHwPiz6xEpHC0WSivb2UN7" className="inline-flex items-center justify-center w-full px-5 py-3 text-sm font-medium text-center text-gray-900 border border-gray-200 rounded-lg sm:w-auto hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800 gap-1">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M.057 24l1.687-6.163c-1.041-1.804-1.588-3.849-1.587-5.946.003-6.556 5.338-11.891 11.893-11.891 3.181.001 6.167 1.24 8.413 3.488 2.245 2.248 3.481 5.236 3.48 8.414-.003 6.557-5.338 11.892-11.893 11.892-1.99-.001-3.951-.5-5.688-1.448l-6.305 1.654zm6.597-3.807c1.676.995 3.276 1.591 5.392 1.592 5.448 0 9.886-4.434 9.889-9.885.002-5.462-4.415-9.89-9.881-9.892-5.452 0-9.887 4.434-9.889 9.884-.001 2.225.651 3.891 1.746 5.634l-.999 3.648 3.742-.981zm11.387-5.464c-.074-.124-.272-.198-.57-.347-.297-.149-1.758-.868-2.031-.967-.272-.099-.47-.149-.669.149-.198.297-.768.967-.941 1.165-.173.198-.347.223-.644.074-.297-.149-1.255-.462-2.39-1.475-.883-.788-1.48-1.761-1.653-2.059-.173-.297-.018-.458.13-.606.134-.133.297-.347.446-.521.151-.172.2-.296.3-.495.099-.198.05-.372-.025-.521-.075-.148-.669-1.611-.916-2.206-.242-.579-.487-.501-.669-.51l-.57-.01c-.198 0-.52.074-.792.372s-1.04 1.016-1.04 2.479 1.065 2.876 1.213 3.074c.149.198 2.095 3.2 5.076 4.487.709.306 1.263.489 1.694.626.712.226 1.36.194 1.872.118.571-.085 1.758-.719 2.006-1.413.248-.695.248-1.29.173-1.414z" /></svg> Join Whatsapp Group
-                            </a>
+                            </a> */}
                             <Link target="_blank" href="https://rzp.io/l/KcgyLNpnnx" className="inline-flex items-center justify-center w-full px-5 py-3 text-sm text-center text-white border bg-purple-400 shadow-lg font-bold rounded-lg sm:w-auto hover:bg-purple-600 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800 gap-2">
-                                <ScanLine/> Buy For ₹1500 Instead
+                                <ScanLine /> Buy For ₹1500
                             </Link>
                         </div>
-                    </div>  
+                    </div>
                     <div className="hidden lg:mt-0 lg:col-span-5 lg:flex">
                         <img src="assets/score-reporting-hero.png" alt="score reporting image" />
                     </div>
@@ -57,11 +57,11 @@ export default function Main() {
                         <img className="hidden w-full mb-4 rounded-lg lg:mb-0 lg:flex" src="assets/score-reporting-feature-2.png" alt="feature image 2" />
                         <div className="text-gray-500 sm:text-lg dark:text-gray-400">
                             <h2 className="mb-4 text-3xl font-extrabold tracking-tight text-gray-900 dark:text-white">How to obtain discount codes</h2>
-                            <p className="mb-8 font-light lg:text-xl">Get step-by-step guidance to complete your college application work efficiently, or if you have already completed it, join to gain access to exclusive TOEFL voucher codes. Our community supports your academic ambitions at every step.</p>
+                            <p className="mb-8 font-light lg:text-xl">To get the discount, click the Buy Now button, and it will take you to the payment page. If you need help with your application work, you can join our MS in US WhatsApp community.</p>
                             <ul role="list" className="pt-8 space-y-5 border-t border-gray-200 my-7 dark:border-gray-700">
                                 <li className="flex space-x-3">
                                     <SvgIcon />
-                                    <span className="text-base font-medium leading-tight text-gray-900 dark:text-white">Free Toefl score reporting codes</span>
+                                    <span className="text-base font-medium leading-tight text-gray-900 dark:text-white">@50% off on Toefl ASR codes</span>
                                 </li>
                                 <li className="flex space-x-3">
                                     <SvgIcon />
@@ -77,10 +77,10 @@ export default function Main() {
                                 </li>
                                 <li className="flex space-x-3">
                                     <SvgIcon />
-                                    <span className="text-base font-medium leading-tight text-gray-900 dark:text-white">Save upto 10,000rs on voucher codes</span>
+                                    <span className="text-base font-medium leading-tight text-gray-900 dark:text-white">Save thousands on voucher codes</span>
                                 </li>
                             </ul>
-                            <p className="font-light lg:text-xl">Instant Access to TOEFL Voucher Codes- Join Our WhatsApp Group</p>
+                            <p className="font-light lg:text-xl">Instant Access to Application Assessment- Join Our WhatsApp Group</p>
                         </div>
                     </div>
                 </div>
--- app/score-reporting/page.tsx
+++ app/score-reporting/page.tsx
@@ -13,44 +13,40 @@ const dataQA = [
         answer: "Simply join our WhatsApp group! We offer free, step-by-step assistance to help you with your application process or contact us at +91 8802-8801-81."
     },
     {
-        question: "Can I still get assistance if I have already completed my application?",
+        question: "Can I get assistance when application is done?",
         answer: "Absolutely! Even if your application is already done, joining our group gives you access to expert assistance and a supportive community."
     },
     {
-        question: "What support can I expect after joining the WhatsApp group?",
+        question: "What support can I expect after joining the group?",
         answer: "Our community provides continuous support for your academic needs, from application tips to study resources for TOEFL preparation."
-    },
-    {
-        question: "Are the TOEFL test voucher codes really free?",
-        answer: "Yes, our TOEFL test voucher codes are provided for free to members of our WhatsApp group as part of our commitment to your educational journey."
     },
 ]
 
 export default function Page() {
     return (
         <>
-        <Seo
-         title = 'TOEFL Score Reporting - Know Your Scores'
-         description = 'Learn how to report your TOEFL scores to universities and institutions. Get all the details here.'
-         url = 'https://toeflgoglobal.com/score-reporting'
-         image='https://www.dropbox.com/scl/fi/efgh6d39t1z69ulz03dl3/GoGlobalSocialShare.jpg?rlkey=o8vttiq065fkpsemyzo04fcj5&raw=1'
-        />
-        <Suspense fallback={<div><LoadingSpinner /></div>}>
-            <div className="">
-                <Main/>
-                <Body>
-                    <H1>FAQ</H1>
-                    {dataQA.map((data, i) =>
-                        <div key={i}>
-                            <QA
-                                question={data.question}
-                                answer={data.answer}
-                            />
-                        </div>
-                    )}
-                </Body>
-            </div>
-        </Suspense>
+            <Seo
+                title='TOEFL Score Reporting - Know Your Scores'
+                description='Learn how to report your TOEFL scores to universities and institutions. Get all the details here.'
+                url='https://toeflgoglobal.com/score-reporting'
+                image='https://www.dropbox.com/scl/fi/efgh6d39t1z69ulz03dl3/GoGlobalSocialShare.jpg?rlkey=o8vttiq065fkpsemyzo04fcj5&raw=1'
+            />
+            <Suspense fallback={<div><LoadingSpinner /></div>}>
+                <div className="">
+                    <Main />
+                    <Body>
+                        <H1>FAQ</H1>
+                        {dataQA.map((data, i) =>
+                            <div key={i}>
+                                <QA
+                                    question={data.question}
+                                    answer={data.answer}
+                                />
+                            </div>
+                        )}
+                    </Body>
+                </div>
+            </Suspense>
         </>
     );
 }

2841120a1dd7303fa90b2066dffee1a1dd535476
apoorv <apoorv@mja.in>
2025-01-26 22:03:43 +0530

    added more fixes for popup

--- components/Head/toefl-community-notification.tsx
+++ components/Head/toefl-community-notification.tsx
@@ -5,11 +5,17 @@ import { motion, AnimatePresence, PanInfo } from "framer-motion"
 import { Button } from "@/components/ui/button"
 import { Card, CardContent } from "@/components/ui/card"
 import { Bell, X } from "lucide-react"
+import { FaWhatsapp } from "react-icons/fa"
 
 export default function ToeflCommunityNotification() {
     const [isVisible, setIsVisible] = useState(false)
 
     useEffect(() => {
+        const isJoined = localStorage.getItem("Whatsap_community_joined");
+        if (isJoined === "true") {
+            return;
+        }
+
         const timer = setTimeout(() => {
             setIsVisible(true);
             const audio = new Audio("/sounds/notification.mp3");
@@ -21,6 +27,7 @@ export default function ToeflCommunityNotification() {
 
 
     const handleJoin = () => {
+        localStorage.setItem("Whatsap_community_joined", "true");
         window.open("https://chat.whatsapp.com/CHwPiz6xEpHC0WSivb2UN7", "_blank")
     }
 
@@ -51,9 +58,9 @@ export default function ToeflCommunityNotification() {
                         <Card className="relative rounded-lg w-[90%] mx-auto mt-1 bg-gradient-to-r from-green-400 via-green-500 to-green-600 shadow-lg">
                             <CardContent className="p-4 flex items-center justify-between">
                                 <div className="flex items-center space-x-4">
-                                    <Bell className="h-6 w-6 text-white animate-bounce" />
+                                    <Bell className="h-7 w-7 text-white animate-bounce" />
                                     <p className="text-white font-semibold text-sm sm:text-base md:text-lg">
-                                        Join TOEFL student community (700+ members)
+                                        Join TOEFL student community (700+ students)
                                     </p>
                                 </div>
                                 <motion.div whileHover={{ scale: 1.05 }} whileTap={{ scale: 0.95 }}>
@@ -61,7 +68,7 @@ export default function ToeflCommunityNotification() {
                                         onClick={handleJoin}
                                         className="bg-white text-green-600 hover:bg-green-100 hover:text-green-700 transition-all duration-300 ease-in-out transform hover:-translate-y-1 hover:shadow-xl"
                                     >
-                                        Join Now
+                                        <FaWhatsapp />
                                     </Button>
                                 </motion.div>
                                 <X onClick={() => { setIsVisible(false) }} className="absolute right-1 top-1 h-4 w-4" />

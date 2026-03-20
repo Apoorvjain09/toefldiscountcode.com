84e6a3afd94df2267830574fab8a84d06a6e95b8
Apoorv <apoorvarnav1@gmail.com>
2026-03-20 18:55:08 +0530

    fix prev dep issues

--- next.config.mjs
+++ next.config.mjs
@@ -2,11 +2,14 @@
 
 import withPWA from 'next-pwa';
 
-const nextConfig = withPWA({
+const pwa = withPWA({
   dest: 'public',
   disable: process.env.NODE_ENV === 'development',
   register: true,
   skipWaiting: true,
 });
 
-export default nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
+
+export default pwa(nextConfig);
